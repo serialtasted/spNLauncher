df5f655e6673eaeaa2cce4ee45afc7fb1ac92bfa
Rodrigo Levy <rodrigo.taveira.levy@gmail.com>
2015-08-01 22:31:00 +0100

    Fixed issue #2
    Fixed issue #2 (Fails to download addons if addons folder is empty)

--- MainForm.cs
+++ MainForm.cs
@@ -567,85 +567,92 @@ namespace spNLauncherArma3
             {
                 lstb_detectedAddons.Items.Clear();
 
-                foreach (string d in Directory.GetDirectories(AddonsFolder))
+                DirectoryInfo addonDir = new DirectoryInfo(AddonsFolder);
+                DirectoryInfo[] subDirs = addonDir.GetDirectories();
+
+                if (subDirs.Length == 0)
                 {
-                    DirectoryInfo dir = new DirectoryInfo(d);
-                    if ((dir.Name.ToLower().Contains("jsrs") || dir.Name.ToLower().Contains("dragonfyre")) && isJSRSAllowed)
-                    {
-                        chb_jsrs.Enabled = true;
-                        chb_jsrs.Tag = Path.GetFileName(d);
-                        break;
-                    }
-                    else { chb_jsrs.Tag = ""; }
+                    chb_jsrs.Tag = "";
+                    chb_blastcore.Tag = "";
                 }
-
-                foreach (string d in Directory.GetDirectories(AddonsFolder))
+                else
                 {
-                    DirectoryInfo dir = new DirectoryInfo(d);
-
-                    if (dir.Name.ToLower().Contains("blastcore") && isBlastcoreAllowed)
+                    foreach (DirectoryInfo dir in addonDir.GetDirectories())
                     {
-                        chb_blastcore.Enabled = true;
-                        chb_blastcore.Tag = Path.GetFileName(d);
-                        break;
-                    }
-                    else { chb_blastcore.Tag = ""; }
-                }
-
-                foreach (string d in Directory.GetDirectories(AddonsFolder))
-                {
-                    DirectoryInfo dir = new DirectoryInfo(d);
-                    bool aux_isInstalled = false;
-                    bool aux_isListed = false;
-
-                    if (dir.Name.StartsWith("@"))
-                    {
-                        foreach (string m in modsName)
+                        if ((dir.Name.ToLower().Contains("jsrs") || dir.Name.ToLower().Contains("dragonfyre")) && isJSRSAllowed)
                         {
-                            if (dir.Name == m || dir.Name.ToLower().Contains("blastcore") || dir.Name.ToLower().Contains("jsrs") || dir.Name.ToLower().Contains("dragonfyre")) { aux_isInstalled = true; break; }
-                            else { continue; }
+                            chb_jsrs.Enabled = true;
+                            chb_jsrs.Tag = Path.GetFileName(dir.Name);
+                            break;
                         }
+                        else { chb_jsrs.Tag = ""; }
+                    }
 
-                        if (!aux_isInstalled)
+                    foreach (DirectoryInfo dir in addonDir.GetDirectories())
+                    {
+                        if (dir.Name.ToLower().Contains("blastcore") && isBlastcoreAllowed)
                         {
-                            foreach (string m in lstb_activeAddons.Items)
+                            chb_blastcore.Enabled = true;
+                            chb_blastcore.Tag = Path.GetFileName(dir.Name);
+                            break;
+                        }
+                        else { chb_blastcore.Tag = ""; }
+                    }
+
+                    foreach (DirectoryInfo dir in addonDir.GetDirectories())
+                    {
+                        bool aux_isInstalled = false;
+                        bool aux_isListed = false;
+
+                        if (dir.Name.StartsWith("@"))
+                        {
+                            foreach (string m in modsName)
                             {
-                                if (dir.Name == m) { aux_isListed = true; break; }
+                                if (dir.Name == m || dir.Name.ToLower().Contains("blastcore") || dir.Name.ToLower().Contains("jsrs") || dir.Name.ToLower().Contains("dragonfyre")) { aux_isInstalled = true; break; }
                                 else { continue; }
                             }
 
-                            if (!aux_isListed)
-                                lstb_detectedAddons.Items.Add(dir.Name);
-                        }
-                    }
-                    else { continue; }
-                }
+                            if (!aux_isInstalled)
+                            {
+                                foreach (string m in lstb_activeAddons.Items)
+                                {
+                                    if (dir.Name == m) { aux_isListed = true; break; }
+                                    else { continue; }
+                                }
 
-                foreach (string f in lstb_activeAddons.Items)
-                {
-                    foreach (string m in modsName)
-                    {
-                        if (f == m)
-                        {
-                            lstb_activeAddons.Items.Remove(f);
-                            break;
-                        }
-                    }
-
-                    bool aux_isInstalled = false;
-
-                    foreach (string d in Directory.GetDirectories(AddonsFolder))
-                    {
-                        if (d.Contains(f))
-                        {
-                            aux_isInstalled = true;
-                            break;
+                                if (!aux_isListed)
+                                    lstb_detectedAddons.Items.Add(dir.Name);
+                            }
                         }
                         else { continue; }
                     }
 
-                    if (!aux_isInstalled)
-                        lstb_activeAddons.Items.Remove(f);
+                    foreach (string f in lstb_activeAddons.Items)
+                    {
+                        foreach (string m in modsName)
+                        {
+                            if (f == m)
+                            {
+                                lstb_activeAddons.Items.Remove(f);
+                                break;
+                            }
+                        }
+
+                        bool aux_isInstalled = false;
+
+                        foreach (string d in Directory.GetDirectories(AddonsFolder))
+                        {
+                            if (d.Contains(f))
+                            {
+                                aux_isInstalled = true;
+                                break;
+                            }
+                            else { continue; }
+                        }
+
+                        if (!aux_isInstalled)
+                            lstb_activeAddons.Items.Remove(f);
+                    }
                 }
             }
             catch
@@ -2002,7 +2009,7 @@ namespace spNLauncherArma3
                             }
                             catch { }
 
-                            awaitTSPlugin = false; throw;
+                            awaitTSPlugin = false; break;
                         }
                     }
                 } while (awaitTSPlugin);
