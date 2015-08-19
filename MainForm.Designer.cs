67313b87146e13ea7ac7fc101844b1830d3b529c
Rodrigo Levy <rodrigo.taveira.levy@gmail.com>
2015-08-19 05:00:59 +0100

    Fixed version tag thingy

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -39,9 +39,11 @@
             this.panel10 = new System.Windows.Forms.Panel();
             this.label30 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
+            this.splitButton1 = new spNLauncherArma3.Controls.SplitButton();
             this.menu_blastcore = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.chb_blastcore = new System.Windows.Forms.ToolStripMenuItem();
             this.btn_downloadBlastcore = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_jsrs = new spNLauncherArma3.Controls.SplitButton();
             this.menu_jsrs = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.chb_jsrs = new System.Windows.Forms.ToolStripMenuItem();
             this.btn_downloadJSRS = new System.Windows.Forms.ToolStripMenuItem();
@@ -158,6 +160,7 @@
             this.btn_Launch = new System.Windows.Forms.PictureBox();
             this.txt_curFile = new System.Windows.Forms.Label();
             this.txt_percentageStatus = new System.Windows.Forms.Label();
+            this.prb_progressBar = new spNLauncherArma3.Controls.Windows7ProgressBar();
             this.label17 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
             this.TitleBar = new System.Windows.Forms.Panel();
@@ -179,9 +182,6 @@
             this.btn_downloadConfigs = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             this.downloadQueue = new System.ComponentModel.BackgroundWorker();
-            this.splitButton1 = new spNLauncherArma3.Controls.SplitButton();
-            this.btn_jsrs = new spNLauncherArma3.Controls.SplitButton();
-            this.prb_progressBar = new spNLauncherArma3.Controls.Windows7ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.sysbtn_minimize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sysbtn_close)).BeginInit();
             this.MainWindow.SuspendLayout();
@@ -276,11 +276,11 @@
             // 
             // Panels
             // 
+            this.Panels.Controls.Add(this.panel_about);
+            this.Panels.Controls.Add(this.panel_help);
             this.Panels.Controls.Add(this.panel_launchOptions);
             this.Panels.Controls.Add(this.panel_community);
             this.Panels.Controls.Add(this.panel_news);
-            this.Panels.Controls.Add(this.panel_about);
-            this.Panels.Controls.Add(this.panel_help);
             this.Panels.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Panels.Location = new System.Drawing.Point(0, 131);
             this.Panels.Name = "Panels";
@@ -340,6 +340,20 @@
             this.label13.TabIndex = 2;
             this.label13.Text = "Audio mod wich enhances over 5500 sound effects";
             // 
+            // splitButton1
+            // 
+            this.splitButton1.AutoSize = true;
+            this.splitButton1.ContextMenuStrip = this.menu_blastcore;
+            this.splitButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.splitButton1.Location = new System.Drawing.Point(9, 42);
+            this.splitButton1.Name = "splitButton1";
+            this.splitButton1.Size = new System.Drawing.Size(91, 23);
+            this.splitButton1.SplitMenuStrip = this.menu_blastcore;
+            this.splitButton1.TabIndex = 1;
+            this.splitButton1.Text = "Blastcore";
+            this.splitButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.splitButton1.UseVisualStyleBackColor = true;
+            // 
             // menu_blastcore
             // 
             this.menu_blastcore.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -365,6 +379,21 @@
             this.btn_downloadBlastcore.Size = new System.Drawing.Size(152, 22);
             this.btn_downloadBlastcore.Text = "Download";
             this.btn_downloadBlastcore.Click += new System.EventHandler(this.btn_downloadBlastcore_Click);
+            // 
+            // btn_jsrs
+            // 
+            this.btn_jsrs.AutoSize = true;
+            this.btn_jsrs.ContextMenuStrip = this.menu_jsrs;
+            this.btn_jsrs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_jsrs.Location = new System.Drawing.Point(9, 13);
+            this.btn_jsrs.Name = "btn_jsrs";
+            this.btn_jsrs.Size = new System.Drawing.Size(91, 23);
+            this.btn_jsrs.SplitMenuStrip = this.menu_jsrs;
+            this.btn_jsrs.TabIndex = 0;
+            this.btn_jsrs.Text = "JSRS";
+            this.btn_jsrs.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_jsrs.UseVisualStyleBackColor = true;
+            this.btn_jsrs.Click += new System.EventHandler(this.btn_JSRS_Click);
             // 
             // menu_jsrs
             // 
@@ -1851,6 +1880,14 @@
             this.txt_percentageStatus.TabIndex = 7;
             this.txt_percentageStatus.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // prb_progressBar
+            // 
+            this.prb_progressBar.ContainerControl = this;
+            this.prb_progressBar.Location = new System.Drawing.Point(22, 50);
+            this.prb_progressBar.Name = "prb_progressBar";
+            this.prb_progressBar.Size = new System.Drawing.Size(718, 23);
+            this.prb_progressBar.TabIndex = 10;
+            // 
             // label17
             // 
             this.label17.AutoSize = true;
@@ -1959,7 +1996,7 @@
             this.btn_reinstallTFRPlugins,
             this.btn_downloadConfigs});
             this.menu_moreOptions.Name = "menu_moreOptions";
-            this.menu_moreOptions.Size = new System.Drawing.Size(209, 92);
+            this.menu_moreOptions.Size = new System.Drawing.Size(209, 70);
             // 
             // menu_RemoteSettings
             // 
@@ -1968,7 +2005,7 @@
             this.btn_showRemoteSettings});
             this.menu_RemoteSettings.Image = global::spNLauncherArma3.Properties.Resources.cog;
             this.menu_RemoteSettings.Name = "menu_RemoteSettings";
-            this.menu_RemoteSettings.Size = new System.Drawing.Size(183, 22);
+            this.menu_RemoteSettings.Size = new System.Drawing.Size(208, 22);
             this.menu_RemoteSettings.Text = "Remote Settings";
             // 
             // btn_reloadRemoteSettings
@@ -1991,7 +2028,7 @@
             // 
             this.btn_reinstallTFRPlugins.Image = global::spNLauncherArma3.Properties.Resources.puzzle_piece;
             this.btn_reinstallTFRPlugins.Name = "btn_reinstallTFRPlugins";
-            this.btn_reinstallTFRPlugins.Size = new System.Drawing.Size(183, 22);
+            this.btn_reinstallTFRPlugins.Size = new System.Drawing.Size(208, 22);
             this.btn_reinstallTFRPlugins.Text = "Reinstall TFR plugins";
             this.btn_reinstallTFRPlugins.Click += new System.EventHandler(this.btn_reinstallTFRPlugins_Click);
             // 
@@ -2016,43 +2053,6 @@
             // 
             this.downloadQueue.DoWork += new System.ComponentModel.DoWorkEventHandler(this.downloadQueue_DoWork);
             this.downloadQueue.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.downloadQueue_RunWorkerCompleted);
-            // 
-            // splitButton1
-            // 
-            this.splitButton1.AutoSize = true;
-            this.splitButton1.ContextMenuStrip = this.menu_blastcore;
-            this.splitButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.splitButton1.Location = new System.Drawing.Point(9, 42);
-            this.splitButton1.Name = "splitButton1";
-            this.splitButton1.Size = new System.Drawing.Size(91, 23);
-            this.splitButton1.SplitMenuStrip = this.menu_blastcore;
-            this.splitButton1.TabIndex = 1;
-            this.splitButton1.Text = "Blastcore";
-            this.splitButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.splitButton1.UseVisualStyleBackColor = true;
-            // 
-            // btn_jsrs
-            // 
-            this.btn_jsrs.AutoSize = true;
-            this.btn_jsrs.ContextMenuStrip = this.menu_jsrs;
-            this.btn_jsrs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_jsrs.Location = new System.Drawing.Point(9, 13);
-            this.btn_jsrs.Name = "btn_jsrs";
-            this.btn_jsrs.Size = new System.Drawing.Size(91, 23);
-            this.btn_jsrs.SplitMenuStrip = this.menu_jsrs;
-            this.btn_jsrs.TabIndex = 0;
-            this.btn_jsrs.Text = "JSRS";
-            this.btn_jsrs.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_jsrs.UseVisualStyleBackColor = true;
-            this.btn_jsrs.Click += new System.EventHandler(this.btn_JSRS_Click);
-            // 
-            // prb_progressBar
-            // 
-            this.prb_progressBar.ContainerControl = this;
-            this.prb_progressBar.Location = new System.Drawing.Point(22, 50);
-            this.prb_progressBar.Name = "prb_progressBar";
-            this.prb_progressBar.Size = new System.Drawing.Size(718, 23);
-            this.prb_progressBar.TabIndex = 10;
             // 
             // MainForm
             // 
